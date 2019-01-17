3be074ce1acf7da878dd312f9434b829fa9fb47d
mdsinalpha <mdsinalpha@localhost.localdomain>
2019-01-17 18:11:04 +0330

    Up & Running with ANTLR java interface :))

---
+++ src/LuluRun.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+/**
+ *
+ * @author mdsinalpha
+ */
+public class LuluRun {
+    
+    public static void main(String[] args){
+        try{
+            File input = new File(args[0]);
+            StringBuilder program = new StringBuilder();
+            Scanner scan = new Scanner(input);
+            while(scan.hasNextLine())
+                program.append(scan.nextLine()).append("\n");
+            LuluLexer lexer = new LuluLexer(new ANTLRInputStream(program.toString()));
+            LuluParser parser = new LuluParser(new CommonTokenStream(lexer));
+            ParseTreeWalker walker = new ParseTreeWalker();
+            LuluSemanticAnalyzer loader = new LuluSemanticAnalyzer();
+            walker.walk(loader, parser.program());
+            System.out.println(loader.test);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
--- src/LuluSemanticAnalyzer.java
+++ src/LuluSemanticAnalyzer.java
@@ -1,8 +1,31 @@
+
+import java.util.ArrayList;
+
 /**
  *
  * @author mdsinalpha
  */
 public class LuluSemanticAnalyzer extends LuluBaseListener {
+    
+    public ArrayList<String> test;
+    
+    public LuluSemanticAnalyzer(){
+        test = new ArrayList<>();
+    }
+    
+    @Override 
+    public void exitExpr(LuluParser.ExprContext ctx){
+        try{
+            if(ctx.children.size()==3){
+                test.add(ctx.expr(0).const_val().INT_CONST().getText()+
+                         ctx.ARIT_P1().getText()+
+                         ctx.expr(1).const_val().INT_CONST().getText());
+            }else test.add(ctx.const_val().REAL_CONST().getText());
+        }
+        catch(Exception e){
+            
+        }
+    }
     
     
 }
