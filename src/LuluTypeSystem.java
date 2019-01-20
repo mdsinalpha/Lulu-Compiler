98ae5430dc84a775e9639d254886128c1a27eff8
mdsinalpha <mdsinalpha@localhost.localdomain>
2019-01-20 18:27:03 +0330

    Type system unary function identation fixed.

--- src/LuluTypeSystem.java
+++ src/LuluTypeSystem.java
@@ -11,19 +11,19 @@ public class LuluTypeSystem{
     public static int UNDEFINED = -10;
     
     public static Integer type(Integer operand, Integer operator){
-         switch(operator){
-             case LuluLexer.UNARY_OP:
-                 if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
-                     return LuluLexer.BOOL_CONST;
-                 break;
-             case LuluLexer.MINUS:
-                 if(operand==LuluLexer.INT_CONST||operand==LuluLexer.REAL_CONST)
-                     return operand;
-                 break;
-             case LuluLexer.BITWISE_NOT:
-                 if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
-                     return LuluLexer.INT_CONST;
-         }
+        switch(operator){
+            case LuluLexer.UNARY_OP:
+                if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
+                    return LuluLexer.BOOL_CONST;
+                break;
+            case LuluLexer.MINUS:
+                if(operand==LuluLexer.INT_CONST||operand==LuluLexer.REAL_CONST)
+                    return operand;
+                break;
+            case LuluLexer.BITWISE_NOT:
+                if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
+                    return LuluLexer.INT_CONST;
+        }
         return UNDEFINED;
     }
    
