7f4fa0a43294e5103a0e79dc519dd3167aeeff1c
mdsinalpha <mdsinalpha@localhost.localdomain>
2019-01-20 18:23:41 +0330

    Type system unary function completed.

--- src/LuluTypeSystem.java
+++ src/LuluTypeSystem.java
@@ -8,13 +8,26 @@ import org.antlr.v4.runtime.ParserRuleContext;
  */
 public class LuluTypeSystem{
     
+    public static int UNDEFINED = -10;
     
-    public Integer type(Integer operand, Integer operator){
-        //TODO
-        return 0;
+    public static Integer type(Integer operand, Integer operator){
+         switch(operator){
+             case LuluLexer.UNARY_OP:
+                 if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
+                     return LuluLexer.BOOL_CONST;
+                 break;
+             case LuluLexer.MINUS:
+                 if(operand==LuluLexer.INT_CONST||operand==LuluLexer.REAL_CONST)
+                     return operand;
+                 break;
+             case LuluLexer.BITWISE_NOT:
+                 if(operand==LuluLexer.BOOL_CONST||operand==LuluLexer.INT_CONST)
+                     return LuluLexer.INT_CONST;
+         }
+        return UNDEFINED;
     }
    
-    public Integer type(Integer operand1, Integer operand2, Integer operator){
+    public static Integer type(Integer operand1, Integer operand2, Integer operator){
         //TODO
         return 0;
     }
