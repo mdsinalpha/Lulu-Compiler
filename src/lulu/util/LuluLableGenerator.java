a9dc19ec6c9616844d885421be6574211359a174
pooriazmn <pooriazmn@gmail.com>
2019-01-25 18:13:20 +0330

    LuluLableGenerator improved to sth reasonable

--- src/lulu/util/LuluLableGenerator.java
+++ src/lulu/util/LuluLableGenerator.java
@@ -16,7 +16,7 @@ public class LuluLableGenerator {
     }
     
     public String getNextLable(){
-        counter = (counter + 1) % 1000000007;
-        return String.format("%s%d%d", starter, System.currentTimeMillis()%10000, counter);
+        counter += 1;
+        return String.format("%s%d", starter, counter);
     }
 }
