987ce7bf3fa0c2b133acca7999baa7cf57455099
mdsinalpha <mdsinalpha@localhost.localdomain>
2019-01-27 18:28:51 +0330

    Removed unused imports.

--- src/lulu/gui/LuluSymbolTableTreeRevealer.java
+++ src/lulu/gui/LuluSymbolTableTreeRevealer.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TreeItem;
